f49b4e68d44238bacbf3e4cc3ba2fb02ec3f2f0c
Ri <ri@ri-desktop>
2015-11-17 18:48:00 +0300

    http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  http_basic_authenticate_with(
+    name: Rails.application.secrets.http_auth_login,
+    password: Rails.application.secrets.http_auth_password)
+
 end
